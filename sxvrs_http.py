856c2ed75fe79dc794ef75ddfdf5b8c4463b1a76
Rustem <zebatus@gmail.com>
2020-03-19 17:50:02 +0300

    start flask

--- sxvrs_http.py
+++ sxvrs_http.py
@@ -34,6 +34,10 @@ from cls.misc import check_topic, Recorder
 
 # Global variables
 recorders = []
+def get_recorder_by_name(name):
+    for recorder in recorders:
+        if recorder.name == name:
+            return recorder
 
 # Get running script name
 script_path, script_name = os.path.split(os.path.splitext(__file__)[0])
@@ -133,9 +137,9 @@ except :
     stored_exception=sys.exc_info()    
 
 
-#########
-from flask import Flask
-flask_app = Flask(__name__)
+#####    Flask HTTP Server   #####
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__, static_url_path='/static', static_folder='/templates/static')
 
 def refresh_recorder_status(recorder=None):
     """This function is for running of refreshment of the status for all cam"""
@@ -148,6 +152,155 @@ def refresh_recorder_status(recorder=None):
         mqtt_client.publish(mqtt_topic_pub.format(source_name='list'))
         logging.debug(f"MQTT publish: {mqtt_topic_pub.format(source_name='list')}")
 
+@app.route('/')
+def page_index():
+    refresh_recorder_status()
+    return 'Hello World'
+
+@app.route('/logs')
+@app.route('/logs/<name>')
+@app.route('/logs/<name>/<max_len>/<page>')
+def page_logs(name=None, page = None, max_len = None):    
+    logs_path = os.path.dirname(cnfg.data['logger']['handlers']['info_file_handler']['filename'])
+    charset = sys.getfilesystemencoding()
+    title = f'Logs: {page}'
+    list_box = ""
+    for file in os.listdir(logs_path):
+        if os.path.isfile(os.path.join(logs_path,file)) and file[-4:] in ['.err','.log']:
+            list_box += f'<li><a href="/logs/{file}">{file}</a></li>'
+    if page==None:
+        log_box = "Please select logs.file"
+    else:
+        try:
+            log_box = ""
+            i = 0
+            if max_len is None:
+                max_len = 500
+            else:
+                max_len = int(max_len)
+            with open(os.path.join(logs_path, page), mode='r', encoding='utf-8') as f:
+                for row in reversed(f.readlines()):
+                    log_box += html.escape(row)
+                    i += 1
+                    if i>max_len:
+                        break
+        except:
+            logger.exception(f'Error in opening logs file: {page}')
+            log_box = 'Error loading log file'
+    render_template(os.path.join('templates', 'logs.html'),
+            charset = charset, 
+            title = title,
+            list_box = list_box,
+            log_box = log_box
+        )
+
+@app.route('/restart/<name>')
+def page_restart(name):
+    if name == "daemon":
+        payload = json.dumps({'cmd':'restart'})
+        mqtt_client.publish(mqtt_topic_pub.format(source_name=name), payload)
+    elif name == "http": # try to restart self (HTTP Server script)
+        try:
+            httpd.server_close()
+            time.sleep(1)
+            args = sys.argv[:]
+            exe = sys.executable
+            logging.info("> "*5 + " Restarting the server " + " <"*5)
+            args.insert(0, sys.executable)
+            if sys.platform == 'win32':
+                args = ['"%s"' % arg for arg in args]
+            os.execv(exe, args)
+        except:
+            logger.exception("Can't restart server")
+    render_template(os.path.join('templates', 'restart.html'), name=name)
+
+@app.route('/recorder/<recorder_name>/snapshot/<width>/<height>')
+def recorder_snapshot(recorder_name, width=None, height=None):
+    # get snapshot name for the recorder
+    filename = cnfg.recorders[recorder_name].filename_snapshot()
+    #TODO: need to resize image
+    app.send_static_file(filename)
+
+@app.route('/recorder/<recorder_name>')
+def view_recorder(recorder_name):
+        """Returns page for given camera"""
+        width=400
+        height=300
+        enc = sys.getfilesystemencoding()
+        # get recorder by name
+        recorder = get_recorder_by_name(recorder_name)
+        title = f'Camera: {recorder.name}'
+        blink = ''
+        if recorder.status == 'stopped':
+            btn_name = 'Start'
+            state_img = 'stop.gif'
+            widget_status = 'widget_status_ok'
+        else:
+            btn_name = 'Stop'
+            if recorder.status == 'started':
+                blink = 'blink'
+                state_img = 'rec.gif'
+                widget_status = 'widget_status_ok'
+            elif recorder.status in ['snapshot','restarting']:
+                state_img = 'state.gif'
+                widget_status = 'widget_status'
+            elif recorder.status == 'error':
+                state_img = 'err.gif'
+                widget_status = 'widget_status_err'
+        if recorder.error_cnt>0:
+            widget_err = 'widget_err' 
+        else:
+            widget_err = ''
+        #show log for selected recorder
+        logs_path = os.path.dirname(cnfg.data['logger']['handlers']['info_file_handler']['filename'])
+        logs_file = 'sxvrs_daemon.log'
+        try:
+            log_box = ""
+            i = 0
+            with open(os.path.join(logs_path, logs_file), mode='r', encoding='utf-8') as f:
+                for row in reversed(f.readlines()):
+                    if recorder.name in row:
+                        log_box += html.escape(row)
+                        i += 1
+                        if i>500:
+                            break
+        except:
+            logger.exception(f'Error in opening logs file: {logs_file}')
+            log_box = 'Error loading log file'
+            content = {
+                "snapshot" : os.path.join(recorder.snapshot, str(width), str(height)),
+                "width" : width,
+                "height" : height,
+                "latest_file" : os.path.basename(recorder.latest_file),
+                "error_cnt" : recorder.error_cnt,
+                "status" : recorder.status,
+                "name" : recorder.name,
+                "btn_name" : btn_name,
+                "blink" : blink,
+                "state_img" : state_img,
+                "widget_status" : widget_status,
+                "widget_err" : widget_err,
+
+                "charset" : enc,
+                "title" : title,
+                "widget" : widget,
+                "log_box" : log_box
+            }
+            render_template(os.path.join('templates', 'restart.html'), content=content)
+
+
+
+@app.route('/recorder/<recorder_name>/start')
+def recorder_start(recorder_name):
+    mqtt_client.publish(mqtt_topic_pub.format(source_name=recorder_name), json.dumps({'cmd':'start'}))
+    time.sleep(2) # sleep before refresh, to give time to update data
+    redirect(url_for(view_recorder))
+
+@app.route('/recorder/<recorder_name>/stop')
+def recorder_stop(recorder_name):
+    mqtt_client.publish(mqtt_topic_pub.format(source_name=recorder_name), json.dumps({'cmd':'stop'}))
+    time.sleep(2) # sleep before refresh, to give time to update data
+    redirect(url_for(view_recorder))
 
 if stored_exception==None:
     # start HTTP Server
@@ -155,10 +308,10 @@ if stored_exception==None:
     is_started = False    
     while not is_started:
         try:
-            flask_app.run(
+            app.run(
                     host = cnfg.http_server_host, 
                     port = cnfg.http_server_port, 
-                    debug = True
+                    #debug = True
                 )        
             is_started = True
         except OSError as e:
