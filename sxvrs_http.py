74904a4a6ac69100b446b6d63cccca0fa6d84009
Rustem <zebatus@gmail.com>
2020-03-19 14:42:43 +0300

    refactor

--- sxvrs_http.py
+++ sxvrs_http.py
@@ -51,14 +51,8 @@ cnfg = config_reader(
         log_filename = 'http'
     )
 
-mqtt_name = cnfg.mqtt_name_http
-mqtt_server_host = cnfg.mqtt_server_host
-mqtt_server_port = cnfg.mqtt_server_port
-mqtt_server_keepalive = cnfg.mqtt_server_keepalive
-mqtt_login = cnfg.mqtt_login
-mqtt_pwd = cnfg.mqtt_pwd
-mqtt_topic_publish_tmpl = cnfg.mqtt_topic_client_publish
-mqtt_topic_subscribe_tmpl = cnfg.mqtt_topic_client_subscribe
+mqtt_topic_pub = cnfg.mqtt_topic_client_publish
+mqtt_topic_sub = cnfg.mqtt_topic_client_subscribe
 
 # Class for VR
 class Recorder():
@@ -72,13 +66,13 @@ class Recorder():
 def mqtt_publish_recorder(recorder, message):
     if isinstance(message, dict):
         message = json.dumps(message)
-    mqtt_client.publish(mqtt_topic_publish_tmpl.format(source_name=recorder.name), message)
-    logging.debug(f"MQTT publish: {mqtt_topic_publish_tmpl.format(source_name=recorder.name)} {message}")
+    mqtt_client.publish(mqtt_topic_pub.format(source_name=recorder.name), message)
+    logging.debug(f"MQTT publish: {mqtt_topic_pub.format(source_name=recorder.name)} {message}")
 
 # MQTT event listener
 def on_mqtt_message(client, userdata, message):
     """Provides reaction on all events received from MQTT broker"""
-    global vr_list, mqtt_topic_publish_tmpl
+    global vr_list, mqtt_topic_pub
     try:
         if len(message.payload)>0:
             payload = json.loads(str(message.payload.decode("utf-8")))
@@ -88,8 +82,8 @@ def on_mqtt_message(client, userdata, message):
                 vr_list = []
                 for name in payload:
                     vr_list.append(Recorder(name))
-                    mqtt_client.publish(mqtt_topic_publish_tmpl.format(source_name=name), json.dumps({'cmd':'status'}))
-                    logger.debug(f"MQTT publish: {mqtt_topic_publish_tmpl.format(source_name=name)} {{'cmd':'status'}}")
+                    mqtt_client.publish(mqtt_topic_pub.format(source_name=name), json.dumps({'cmd':'status'}))
+                    logger.debug(f"MQTT publish: {mqtt_topic_pub.format(source_name=name)} {{'cmd':'status'}}")
             # topic = <recorder_name>
             else:
                 for vr in vr_list:
@@ -109,34 +103,34 @@ def on_mqtt_connect(client, userdata, flags, rc):
     client.connection_rc = rc
     if rc==0:
         client.is_connected = True
-        logger.info(f"Connected to MQTT: {mqtt_server_host}:{mqtt_server_port} rc={str(rc)}")
-        mqtt_client.subscribe(mqtt_topic_subscribe_tmpl.format(source_name='#'))  
-        logger.debug(f"MQTT subscribe: {mqtt_topic_subscribe_tmpl.format(source_name='#')}")
+        logger.info(f"Connected to MQTT: {cnfg.mqtt_server_host}:{cnfg.mqtt_server_port} rc={str(rc)}")
+        mqtt_client.subscribe(mqtt_topic_sub.format(source_name='#'))  
+        logger.debug(f"MQTT subscribe: {mqtt_topic_sub.format(source_name='#')}")
         time.sleep(1)
-        mqtt_client.publish(mqtt_topic_publish_tmpl.format(source_name='list'))
-        logger.debug(f"MQTT publish: {mqtt_topic_publish_tmpl.format(source_name='list')}")
+        mqtt_client.publish(mqtt_topic_pub.format(source_name='list'))
+        logger.debug(f"MQTT publish: {mqtt_topic_pub.format(source_name='list')}")
     else:
         logging.error(f"MQTT connection failure with code={rc}")
 
 # setup MQTT connection
 try:
-    mqtt_client = mqtt.Client(mqtt_name) #create new instance
+    mqtt_client = mqtt.Client(cnfg.mqtt_name_http) #create new instance
     mqtt_client.is_connected = False
     mqtt_client.connection_rc = 3
     mqtt_client.enable_logger(logger)
     mqtt_client.on_message=on_mqtt_message #attach function to callback
     mqtt_client.on_connect=on_mqtt_connect #attach function to callback
     #try to connect to broker in a loop, until server becomes available
-    logger.debug(f"host={mqtt_server_host}, port={mqtt_server_port}, keepalive={mqtt_server_keepalive}")
+    logger.debug(f"host={cnfg.mqtt_server_host}, port={cnfg.mqtt_server_port}, keepalive={cnfg.mqtt_server_keepalive}")
     mqtt_client.loop_start() 
-    if mqtt_login!=None: 
-        mqtt_client.username_pw_set(mqtt_login, mqtt_pwd)
+    if cnfg.mqtt_login!=None: 
+        mqtt_client.username_pw_set(cnfg.mqtt_login, cnfg.mqtt_pwd)
     while mqtt_client.connection_rc==3:
         try:
             logger.info(f"Try to connect to MQTT Server..")                
-            mqtt_client.connect(mqtt_server_host, 
-                port = mqtt_server_port,
-                keepalive = mqtt_server_keepalive
+            mqtt_client.connect(cnfg.mqtt_server_host, 
+                port = cnfg.mqtt_server_port,
+                keepalive = cnfg.mqtt_server_keepalive
                 ) 
             while not mqtt_client.is_connected: # blocking code untill connection
                 time.sleep(1)
@@ -146,7 +140,7 @@ try:
             logger.info(f"Server is offline. Wait {wait} sec before retry")
             time.sleep(wait) # if server is not available, then wait for it
 except :
-    logger.exception(f"Can't connect to MQTT broker at address: {mqtt_server_host}:{mqtt_server_port}")
+    logger.exception(f"Can't connect to MQTT broker at address: {cnfg.mqtt_server_host}:{cnfg.mqtt_server_port}")
     stored_exception=sys.exc_info()    
 
 
@@ -162,8 +156,8 @@ def refresh_recorder_status(recorder=None):
             mqtt_publish_recorder(recorder, {'cmd':'status'})
     else:
         mqtt_publish_recorder(None, {'cmd':'status'})
-        mqtt_client.publish(mqtt_topic_publish_tmpl.format(source_name='list'))
-        logging.debug(f"MQTT publish: {mqtt_topic_publish_tmpl.format(source_name='list')}")
+        mqtt_client.publish(mqtt_topic_pub.format(source_name='list'))
+        logging.debug(f"MQTT publish: {mqtt_topic_pub.format(source_name='list')}")
 
 
 if stored_exception==None:
