5f53a09e01b12892564d6c5e2cd1815f5d608ded
Rustem Sharipov <zebatus@gmail.com>
2020-03-23 21:42:46 +0300

    correct timezone

--- docker_cont_run_it.sh
+++ docker_cont_run_it.sh
@@ -6,6 +6,7 @@ docker run -it \
   --name sxvrs \
   --shm-size=256m \
   -p 8282:8282/tcp \
+  -v /etc/localtime:/etc/localtime:ro \
   -v /opt/sxvrs/cnfg:/opt/sxvrs/cnfg \
   -v /opt/sxvrs/storage:/opt/sxvrs/storage \
   -v /opt/sxvrs/logs:/opt/sxvrs/logs \
